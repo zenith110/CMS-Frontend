6fe4aed4463b688ac0c25aa1005656146ed45383
zenith110 <thequake@live.com>
2023-02-10 07:57:21 -0500

    Added basic sign in and login

---
+++ frontend/frontend/src/config.tsx
@@ -0,0 +1,13 @@
+import EmailPassword from "supertokens-auth-react/recipe/emailpassword";
+import Session from "supertokens-auth-react/recipe/session";
+
+export const SuperTokensConfig = {
+    appInfo: {
+        appName: "SuperTokens Demo App",
+        apiDomain: "http://localhost:3001",
+        websiteDomain: "http://localhost:3000",
+    },
+    // recipeList contains all the modules that you want to
+    // use from SuperTokens. See the full list here: https://supertokens.com/docs/guides
+    recipeList: [EmailPassword.init(), Session.init()],
+};
